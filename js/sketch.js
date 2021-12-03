e8927f9a00e6b6bfa3f5ddda8ad3f3ec72577d7c
kieranCoopr <93775394+kieranCoopr@users.noreply.github.com>
2021-12-03 17:01:14 +0000

    finished winter wonderland

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,47 @@
+var img;
+
+const snowFlakeCount = 500;
+const size = 4;
+const gravity = 5;
+
+var snowFlakes = [];
+
+function preload(){
+     img = loadImage('img/Winter.jpg')
+}
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(1280, 720);
     canvas.parent("p5container");
+   
+    
+
+    for(let i = 0; i < snowFlakeCount; i++){
+        
+        snowFlakes.push({
+            x: random(width),
+            y: random(height),
+        });
+
+        console.log(snowFlakes);
+    }
 }
 
 function draw() {
-    background(220);
+    image(img, 0, 0, width, height);
+    noStroke();
+    fill(235,235,255);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    for(let i = 0; i < snowFlakes.length; i++){
+        var snowflake = snowFlakes[i];
+        ellipse(snowflake.x, snowflake.y, size);
+        if(snowflake.y > height + size){
+            snowflake.y = -size;
+        }else{
+            snowflake.y += gravity;
+        }
+    }
+    
+    
+
 }
